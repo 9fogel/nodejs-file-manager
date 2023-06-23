6c20440c7a52795192b072c793000a15696cc1cf
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-06-23 16:42:24 +0300

    feat: add readline prompt message

--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import * as readline from 'readline/promises';
 import { stdin as input, stdout as output } from 'process';
 import { sayHi, sayBye } from "./cli/parseUsername.js";
 import { parseUserInput } from './utils/parseUserInput.js';
+import { showHomeDir } from './os/showHomeDir.js';
 
 sayHi();
+
+let currentPosition = showHomeDir();
 
 const rl = readline.createInterface({ input, output });
 
 rl.on('line', (input) => {
   // console.log(`Received: ${input}`);
+
   if (input.trim() === '.exit') {
     sayBye();
     rl.close();
     process.exit();
   } else {
     parseUserInput(input);
+    rl.setPrompt(currentPosition);
+    rl.prompt();
   }
 });
 
